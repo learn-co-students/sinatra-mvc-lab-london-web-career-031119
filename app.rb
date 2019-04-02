eb27b8d7734926fac22663bdfa3d55c3dde5e98e
azamkhanuk <azamkhandev@gmail.com>
2019-04-02 18:05:56 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :user_input
+      end
+
+    post '/piglatinize' do
+        app = PigLatinizer.new
+        @piglatin = app.piglatinize(params[:user_phrase])
+        erb :piglatinize
+    end
+
+
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+
+class PigLatinizer
+    def piglatinize_word(word)
+        if !!word.capitalize.match(/\A+[AEIOU]/)
+            "#{word}way"
+        else
+           i = word.index(/[aeiou]/)
+           after_v = word[i..word.length]
+           other_side = word.split(/[aeiou]/).first
+           after_v + other_side + "ay"
+        end
+    end
+    
+    def piglatinize(text)
+        array = text.split(" ")
+        array_02 = array.map {|x| piglatinize_word(x)}
+        array_02.join(" ")
+    end
+end
